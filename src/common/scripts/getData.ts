76b96e7da7b6c62f4e531d054ad2a0b74f7c9814
Metaldev <fernydiaz62@gmail.com>
2024-06-16 00:18:01 -0300

    Add: more time in script

--- src/common/scripts/getData.ts
+++ src/common/scripts/getData.ts
@@ -8,54 +8,52 @@ export const getData = async () => {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const page = await browser.newPage();
+  await page.goto(url);
 
-  try {
-    await page.goto(url);
-    await page.waitForSelector('.pagination', { timeout: 60000 });
+  await page.waitForSelector('.pagination', { timeout: 60000 });
 
-    const totalPages = await page.evaluate(() => {
-      const paginationItems = document.querySelectorAll(
-        '.pagination li a.page-linkben span',
-      );
-      return paginationItems.length;
+  const totalPages = await page.evaluate(() => {
+    const paginationItems = document.querySelectorAll(
+      '.pagination li a.page-linkben span',
+    );
+    return paginationItems.length;
+  });
+
+  let allData: {
+    title: string | null;
+    imgUrl: string | null;
+    url: string | null;
+  }[] = [];
+
+  // Iterar sobre cada página
+  for (let i = 1; i <= totalPages; i++) {
+    await page.goto(`https://serviclub.com.ar/blogs/category/page/${i}.html`);
+
+    await page.waitForSelector('.containerBen');
+
+    const data = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('.itemBen')).map((item) => {
+        const titleElement = item.querySelector('h4.tituloListBen');
+        const imgElement = item.querySelector(
+          'img.imgben',
+        ) as HTMLImageElement | null;
+        const urlElement = item.querySelector('a') as HTMLAnchorElement | null;
+
+        const title = titleElement ? titleElement.textContent.trim() : null;
+        const imgUrl = imgElement ? imgElement.src : null;
+        const url = urlElement ? urlElement.href : null;
+
+        return {
+          title,
+          imgUrl,
+          url,
+        };
+      });
     });
 
-    let allData: {
-      title: string | null;
-      imgUrl: string | null;
-      url: string | null;
-    }[] = [];
-
-    for (let i = 1; i <= totalPages; i++) {
-      await page.goto(`https://serviclub.com.ar/blogs/category/page/${i}.html`);
-      await page.waitForSelector('.containerBen');
-
-      const data = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('.itemBen')).map((item) => {
-          const titleElement = item.querySelector('h4.tituloListBen');
-          const imgElement = item.querySelector(
-            'img.imgben',
-          ) as HTMLImageElement | null;
-          const urlElement = item.querySelector(
-            'a',
-          ) as HTMLAnchorElement | null;
-
-          const title = titleElement ? titleElement.textContent.trim() : null;
-          const imgUrl = imgElement ? imgElement.src : null;
-          const url = urlElement ? urlElement.href : null;
-
-          return { title, imgUrl, url };
-        });
-      });
-
-      allData = allData.concat(data);
-    }
-
-    return allData;
-  } catch (error) {
-    console.error('Error en getData:', error);
-    throw new Error('Error al obtener datos de promociones');
-  } finally {
-    await browser.close();
+    allData = allData.concat(data);
   }
+
+  await browser.close();
+  return allData;
 };
